dd72d972e0820895fb30edc29fe9465b29c51874
Samsara-sd <46043133+Samsara-sd@users.noreply.github.com>
2019-01-01 23:31:53 +0800

    Update 1003 Emergency.cpp

--- Advanced Level/1003 Emergency.cpp
+++ Advanced Level/1003 Emergency.cpp
@@ -14,9 +14,9 @@
 
 using namespace std;
 
-const int infinity = 200000000;
+const int infinity = 200000000; // 表示不连通
 
-int find_nearest_city(int city_num, const vector<int>& dist, const vector<bool>& sure)
+int find_nearest_city(int city_num, const vector<int>& dist, const vector<bool>& sure) // 找下一个单源最短距离点
 {
     int ans = -1;
     int temp_min = infinity;
@@ -42,19 +42,19 @@ void Dijkstra(int city_num, int source, int destination, vector<vector<int>>& gr
         sure[this_city] = true;
         if( pre[this_city] != -1 )
         {
-            team_num[this_city] += team_num[pre[this_city]];
+            team_num[this_city] += team_num[pre[this_city]]; // 更新救援队数量, 为其pre点的队数加该点已有的队数
         }
         for (int iter = 0; iter < city_num; iter++)
         {
             if( !sure[iter] && graph[this_city][iter] != infinity )
             {
-                if( dist[this_city] + graph[this_city][iter] < dist[iter] )
+                if( dist[this_city] + graph[this_city][iter] < dist[iter] ) // 找到了更短的路, 更新距离以及路径数, 其pre点也要变化
                 {
                     pre[iter] = this_city;
                     dist[iter] = dist[this_city] + graph[this_city][iter];
                     count[iter] = count[this_city];
                 }
-                else if( dist[this_city] + graph[this_city][iter] == dist[iter] )
+                else if( dist[this_city] + graph[this_city][iter] == dist[iter] ) // 找到了等距的路, 判断是否要改pre点
                 {
                     pre[iter] = ( team_num[pre[iter]] > team_num[this_city] ? pre[iter] : this_city );
                     count[iter] += count[this_city];
@@ -76,7 +76,7 @@ int main(int argc, const char* argv[])
     int city_num, road_num, source, destination;
     cin >> city_num >> road_num >> source >> destination;
     
-    for (int iter = 1; iter <= city_num; iter++)
+    for (int iter = 1; iter <= city_num; iter++) // 各vector的初始化
     {
         vector<int> temp_row;
         for (int jter = 1; jter <= city_num; jter++)
@@ -94,7 +94,7 @@ int main(int argc, const char* argv[])
         team_num.emplace_back(temp_num);
     }
 
-    for (int iter = 1; iter <= road_num; iter++)
+    for (int iter = 1; iter <= road_num; iter++) // 图的初始化
     {
         int temp_s, temp_e, temp_l;
         cin >> temp_s >> temp_e >> temp_l;
